906a5835e8f11c7963ac54f16914ff165673d8b5
tan <t.songglod@gmail.com>
2021-09-07 22:31:36 +0700

    [EDIT] change servlet name

--- simpleweb/src/main/java/sit/int202/simpleweb/servlets/StudentListServlet.java
+++ simpleweb/src/main/java/sit/int202/simpleweb/servlets/StudentListServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 import sit.int202.simpleweb.models.StudentRepository;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet", value = "/student-list")
+@WebServlet(name = "StudentListServlet", value = "/student-list")
 public class StudentListServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
