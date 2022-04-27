cdc4507b8ac85c8b94081af34dfa830c72f20722
tan <t.songglod@gmail.com>
2022-04-28 02:22:43 +0700

    [+] add category GET method

--- oasip-test/src/main/java/sit/int221/oasiptest/controllers/EventCategoryController.java
+++ oasip-test/src/main/java/sit/int221/oasiptest/controllers/EventCategoryController.java
@@ -1,10 +1,8 @@
 package sit.int221.oasiptest.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import sit.int221.oasiptest.dto.EventCategoryDto;
-import sit.int221.oasiptest.entities.EventCategory;
+import sit.int221.oasiptest.dto.api.CategoryDto;
 import sit.int221.oasiptest.services.EventCategoryService;
 
 import java.util.List;
@@ -12,4 +10,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class EventCategoryController {
+    private final EventCategoryService service;
+
+    @Autowired
+    public EventCategoryController(EventCategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<CategoryDto> getCategories() {
+        return service.getCategories();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryDto getCategoryById(@PathVariable Integer id) {
+        return service.getCategoryById(id);
+    }
 }
--- oasip-test/src/main/java/sit/int221/oasiptest/services/EventCategoryService.java
+++ oasip-test/src/main/java/sit/int221/oasiptest/services/EventCategoryService.java
@@ -1,25 +1,36 @@
 package sit.int221.oasiptest.services;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import sit.int221.oasiptest.dto.EventCategoryDto;
+import sit.int221.oasiptest.dto.api.CategoryDto;
 import sit.int221.oasiptest.entities.EventCategory;
 import sit.int221.oasiptest.repo.EventCategoryRepository;
+import sit.int221.oasiptest.utils.ListMapper;
+
+import java.util.List;
 
 @Service
 public class EventCategoryService {
     private final EventCategoryRepository repo;
     private final ModelMapper modelMapper;
+    private final ListMapper listMapper;
 
-    public EventCategoryService(EventCategoryRepository repo, ModelMapper modelMapper) {
+    @Autowired
+    public EventCategoryService(EventCategoryRepository repo, ModelMapper modelMapper, ListMapper listMapper) {
         this.repo = repo;
         this.modelMapper = modelMapper;
+        this.listMapper = listMapper;
     }
 
-    public EventCategoryDto getEventCategoryById(Integer id) {
+    public List<CategoryDto> getCategories() {
+        return listMapper.mapList(repo.findAll(), CategoryDto.class, modelMapper);
+    }
+
+    public CategoryDto getCategoryById(Integer id) {
         EventCategory category = repo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        return modelMapper.map(category, EventCategoryDto.class);
+        return modelMapper.map(category, CategoryDto.class);
     }
 }
